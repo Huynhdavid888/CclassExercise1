b7bb6ffa231e971e5e275d912b90307987e884b3
Huynhdavid888 <huynhdavid1245@gmail.com>
2021-06-22 01:30:45 -0700

    initial commit

---
+++ C Classes Exercise 1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace C_Classes_Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            car myCar = new car();
+            myCar.make = "Ford";
+            myCar.model = "Mustang";
+            myCar.year = 2014;
+
+        }
+    }
+}
